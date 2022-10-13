fb4fa63bb16ed65b97396cdbaff6fc01cc73ee81
visualdemon <wilberamp@gmail.com>
2022-10-12 23:44:00 -0500

    inico p2.1

---
+++ consultas-pruebas.sql
@@ -0,0 +1,24 @@
+# INSERT INTO `ecoapp`.`usuario` (`usuario`, `pass`, `nombre`, `email`, `telefono`, `sexo`, `ocupacion`, `entidad`, `pais`, `ciudad`, `estado`) VALUES ('wil', '123', 'Wil', 'wilberamp@gmail.com', '3206100639', 'M', 'Estudiante', 'Comfa', 'Col', 'Pas', '1');
+
+SELECT * FROM usuario;
+
+SELECT * FROM aporte WHERE id_usuario = 1;
+
+INSERT INTO `ecoapp`.`usuario` (`usuario`, `pass`, `nombre`, `email`, `telefono`, `sexo`, `ocupacion`, `entidad`, `pais`, `ciudad`, `estado`) VALUES ('ale', '345', 'Aleja', 'aleja0206@gmail.com', '3206100639', 'F', 'Estudiante', 'Geografía', 'Col', 'Pas', '1');
+
+INSERT INTO `ecoapp`.`aporte` (`cliente`, `correos-eliminados`, `espacio-liberado`, `id_usuario`, `fecha`) VALUES ('hotmail', '25', '50', '2', CURRENT_TIMESTAMP());
+
+SELECT * FROM aporte WHERE id_usuario = 2;
+
+SELECT usuario.nombre, aporte.cliente FROM usuario, aporte;
+
+SELECT usuario.nombre, aporte.id_usuario FROM usuario, aporte;
+
+SELECT * FROM usuario 
+INNER JOIN aporte ON aporte.id_usuario=usuario.id_usuario;
+
+SELECT nombre, cliente, correos_eliminados, espacio_liberado, fecha FROM aporte
+INNER JOIN usuario
+WHERE aporte.id_usuario=usuario.id_usuario AND aporte.id_usuario=2;
+
+
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Un Click Por Mi Planeta</title>
+</head>
+<body>
+    <h3>Registros</h3>
+    <table border="1" width="80%" cellspacing="0"> 
+        <tr>
+            <td>Cliente</td>
+            <td>Número de correos eliminados</td>
+            <td>Espacio liberado En Mbs.</td>
+            <td>Editar</td>
+            <td>Eliminar</td>
+        </tr>
+    </table>
+</body>
+</html>
+
+<?php 
+
+?>
